165039a80aa96f3810a95e460385cc7e0d9c9312
taitangamer <salimandal123@gmail.com>
2024-09-03 12:47:09 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', '𝐓αιταи ꭙㅤ [•ɢαмєя•]™ 🎫');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'TaitanGamer');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '6060534504');
+
+if (!defined('BOT_NAME'))               define('BOT_NAME', '➠ 𝗧𝗮𝗶𝘁𝗮𝗻 ┃𝗖𝗖┃ 𝗖 𝗛 𝗞 ❗');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'Taitan_Chk_Bot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '6886370123:AAFh1aBlhbmoFzSBFwCLZ-AdbcIngdBI8PI');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-4534577738');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1001938000028');
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'u241258339_sunny');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'u241258339_sunny');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'w~Sij4N?');
